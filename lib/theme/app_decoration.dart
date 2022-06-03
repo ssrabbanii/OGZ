5f2d681c1fdffe5b2a80a5c3e59c14c7d9ffb247
Sadman Sakib Rabbani <61560265+ssrabbanii@users.noreply.github.com>
2022-06-03 11:42:21 +0800

    Add files via upload

---
+++ lib/theme/app_decoration.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+import 'package:ogz_mobile/core/app_export.dart';
+
+class AppDecoration {
+  static BoxDecoration get groupstyleblack1 => BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.31,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylecornerradius => BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            16.00,
+          ),
+        ),
+        border: Border.all(
+          color: ColorConstant.black900,
+          width: getHorizontalSize(
+            1.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylelightGreen1 => BoxDecoration(
+        color: ColorConstant.lightGreenA100,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            12.00,
+          ),
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanregular121 =>
+      BoxDecoration(
+        color: ColorConstant.bluegray100,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylelightGreenA100cornerradius =>
+      BoxDecoration(
+        color: ColorConstant.lightGreenA100,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanregular161 =>
+      BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+        border: Border.all(
+          color: ColorConstant.black900,
+          width: getHorizontalSize(
+            1.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylewhiteA700 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+      );
+  static BoxDecoration get textstyleplusjakartasansromanregular164 =>
+      BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+        border: Border.all(
+          color: ColorConstant.black900,
+          width: getHorizontalSize(
+            1.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylecornerradius3 => BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment(
+            0.999049090292266,
+            0.03581158897356995,
+          ),
+          end: Alignment(
+            5.518453649600019e-8,
+            1.05187201214891,
+          ),
+          colors: [
+            ColorConstant.cyan200,
+            ColorConstant.green100,
+          ],
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanregular165 =>
+      BoxDecoration(
+        color: ColorConstant.bluegray100,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylecornerradius2 => BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            26.00,
+          ),
+        ),
+        border: Border.all(
+          color: ColorConstant.black900,
+          width: getHorizontalSize(
+            2.00,
+          ),
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanregular166 =>
+      BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylecornerradius1 => BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            14.50,
+          ),
+        ),
+        border: Border.all(
+          color: ColorConstant.black900,
+          width: getHorizontalSize(
+            2.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylewhite1 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            54.00,
+          ),
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanregular16 =>
+      BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylegray50 => BoxDecoration(
+        color: ColorConstant.gray50,
+      );
+  static BoxDecoration get groupstyle1 => BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment(
+            0.952318387143696,
+            0.18447999379877644,
+          ),
+          end: Alignment(
+            -0.25460317941776434,
+            1.2124268539789893,
+          ),
+          colors: [
+            ColorConstant.orange100,
+            ColorConstant.teal200,
+            ColorConstant.whiteA700,
+          ],
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanmedium16 =>
+      BoxDecoration(
+        color: ColorConstant.lightGreenA100,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstyle2 => BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment(
+            0.5,
+            -3.0616171314629196e-17,
+          ),
+          end: Alignment(
+            0.5,
+            0.9999999999999999,
+          ),
+          colors: [
+            ColorConstant.yellow20087,
+            ColorConstant.lightGreen40087,
+            ColorConstant.whiteA70087,
+          ],
+        ),
+      );
+  static BoxDecoration get groupstylebluegray100cornerradius => BoxDecoration(
+        color: ColorConstant.bluegray100,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(
+            getHorizontalSize(
+              20.00,
+            ),
+          ),
+          topRight: Radius.circular(
+            getHorizontalSize(
+              20.00,
+            ),
+          ),
+          bottomLeft: Radius.circular(
+            getHorizontalSize(
+              0.00,
+            ),
+          ),
+          bottomRight: Radius.circular(
+            getHorizontalSize(
+              0.00,
+            ),
+          ),
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanmedium162 =>
+      BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+        border: Border.all(
+          color: ColorConstant.black900,
+          width: getHorizontalSize(
+            1.00,
+          ),
+        ),
+      );
+  static BoxDecoration get textstyleplusjakartasansromanmedium161 =>
+      BoxDecoration(
+        color: ColorConstant.deepOrangeA100,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstyleblack900cornerradius => BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            29.50,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylegray300cornerradius => BoxDecoration(
+        color: ColorConstant.gray300,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            14.50,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylewhiteA700cornerradius => BoxDecoration(
+        color: ColorConstant.whiteA700,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstylebluegray10087cornerradius => BoxDecoration(
+        color: ColorConstant.bluegray10087,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstyleblack900 => BoxDecoration(
+        color: ColorConstant.black900,
+      );
+  static BoxDecoration get groupstyleblack3 => BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            20.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstyleblack2 => BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            16.00,
+          ),
+        ),
+      );
+  static BoxDecoration get groupstyleblack4 => BoxDecoration(
+        color: ColorConstant.black900,
+        borderRadius: BorderRadius.circular(
+          getHorizontalSize(
+            180.00,
+          ),
+        ),
+      );
+}
---
+++ lib/theme/app_style.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/material.dart';
+import 'package:ogz_mobile/core/app_export.dart';
+
+class AppStyle {
+  static TextStyle textstyleplusjakartasansromansemibold14 =
+      textstyleplusjakartasansromansemibold161.copyWith(
+    fontSize: getFontSize(
+      14,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold16 =
+      textstyleplusjakartasansromansemibold161.copyWith(
+    color: ColorConstant.black900,
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold10 =
+      textstyleplusjakartasansromansemibold20.copyWith(
+    fontSize: getFontSize(
+      10,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold12 =
+      textstyleplusjakartasansromansemibold121.copyWith(
+    color: ColorConstant.bluegray400,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Plus Jakarta Sans',
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular101 =
+      textstyleplusjakartasansromanregular202.copyWith(
+    fontSize: getFontSize(
+      10,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular102 =
+      textstyleplusjakartasansromanregular201.copyWith(
+    fontSize: getFontSize(
+      10,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular103 =
+      textstyleplusjakartasansromanregular102.copyWith(
+    color: ColorConstant.greenA700,
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular121 =
+      textstyleplusjakartasansromanregular12.copyWith();
+
+  static TextStyle textstyleplusjakartasansromanregular122 =
+      textstyleplusjakartasansromanregular12.copyWith(
+    color: ColorConstant.black901,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular161 =
+      textstyleplusjakartasansromanregular165.copyWith();
+
+  static TextStyle textstyleplusjakartasansromanregular162 =
+      textstyleplusjakartasansromanregular202.copyWith(
+    fontSize: getFontSize(
+      16,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular163 =
+      textstyleplusjakartasansromansemibold162.copyWith(
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular10 =
+      textstyleplusjakartasansromanregular161.copyWith(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      10,
+    ),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular164 =
+      textstyleplusjakartasansromanregular165.copyWith();
+
+  static TextStyle textstyleplusjakartasansromansemibold40 =
+      textstyleplusjakartasansromansemibold401.copyWith(
+    color: ColorConstant.black900,
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold201 =
+      textstyleplusjakartasansromansemibold203.copyWith(
+    color: ColorConstant.greenA700,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular165 =
+      textstyleplusjakartasansromanregular202.copyWith(
+    fontSize: getFontSize(
+      16,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular12 =
+      textstyleplusjakartasansromanregular202.copyWith(
+    fontSize: getFontSize(
+      12,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold202 =
+      textstyleplusjakartasansromansemibold162.copyWith(
+    fontSize: getFontSize(
+      20,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular166 =
+      textstyleplusjakartasansromanregular16.copyWith();
+
+  static TextStyle textstyleplusjakartasansromansemibold203 =
+      textstyleplusjakartasansromansemibold202.copyWith(
+    color: ColorConstant.gray800,
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle textstyleplusjakartasansromanbold12 =
+      textstyleplusjakartasansromanmedium121.copyWith(
+    color: ColorConstant.black900,
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular16 =
+      textstyleplusjakartasansromansemibold162.copyWith(
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium20 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      20,
+    ),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular20 =
+      textstyleplusjakartasansromanregular202.copyWith(
+    color: ColorConstant.greenA700,
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium16 =
+      textstyleplusjakartasansromanmedium161.copyWith();
+
+  static TextStyle textstyleplusjakartasansromansemibold121 =
+      textstyleplusjakartasansromanbold12.copyWith(
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold101 =
+      textstyleplusjakartasansromanregular10.copyWith(
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle textstyleregular8 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      8,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium165 =
+      textstyleplusjakartasansromanmedium20.copyWith(
+    fontSize: getFontSize(
+      16,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium164 =
+      textstyleplusjakartasansromanmedium12.copyWith(
+    fontSize: getFontSize(
+      16,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium163 =
+      textstyleplusjakartasansromanregular164.copyWith(
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium162 =
+      textstyleplusjakartasansromansemibold162.copyWith(
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium161 =
+      textstyleplusjakartasansromanmedium163.copyWith(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Plus Jakarta Sans',
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium12 =
+      textstyleplusjakartasansromanmedium121.copyWith(
+    color: ColorConstant.gray500,
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold20 =
+      textstyleplusjakartasansromanmedium20.copyWith(
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle textstyleplusjakartasansromanmedium121 =
+      textstyleplusjakartasansromanmedium20.copyWith(
+    fontSize: getFontSize(
+      12,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold401 =
+      textstyleplusjakartasansromansemibold162.copyWith(
+    fontSize: getFontSize(
+      40,
+    ),
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular201 =
+      textstyleplusjakartasansromanregular202.copyWith(
+    color: ColorConstant.whiteA700,
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold162 =
+      textstyleplusjakartasansromansemibold161.copyWith(
+    color: ColorConstant.whiteA700,
+  );
+
+  static TextStyle textstyleplusjakartasansromansemibold161 = TextStyle(
+    color: ColorConstant.gray400,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle textstyleplusjakartasansromanregular202 =
+      textstyleplusjakartasansromanmedium20.copyWith(
+    fontWeight: FontWeight.w400,
+  );
+}
